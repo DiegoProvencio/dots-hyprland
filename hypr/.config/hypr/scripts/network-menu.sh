8bc09fb00b11d6a1d6e5b13231d0e1394eeb712b
DiegoProvencio <diegoprovenciomoreno@gmail.com>
2026-03-10 13:00:55 +0100

    update hyprland config + add fastfetch rofi waybar

--- hypr/.config/hypr/scripts/network-menu.sh
+++ hypr/.config/hypr/scripts/network-menu.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+pkill -SIGUSR1 waybar
 
-choice=$(printf "WiFi\nBluetooth" | wofi --dmenu --prompt "Red")
+choice=$(~/dotfiles/rofi/.config/rofi/scripts/network-modi.sh | rofi \
+    -dmenu \
+    -p "" \
+    -show-icons \
+    -mesg "Esc para salir" \
+    -theme ~/.config/rofi/network.rasi)
 
-case $choice in
+pkill -SIGUSR1 waybar
+
+case "$choice" in
     WiFi) kitty -e nmtui ;;
     Bluetooth) kitty -e bluetoothctl ;;
 esac
---
+++ rofi/.config/rofi/scripts/network-modi.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo -e "WiFi\0icon\x1f/home/fokus/.config/rofi/icons/wifi.png"
+echo -e "Bluetooth\0icon\x1f/home/fokus/.config/rofi/icons/bluetooth.png"
